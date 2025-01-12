0a9a9ca8fbbddf4199eae7cb56e6858800caae43
navajoknow <mustyuzhanin@gmail.com>
2025-01-12 19:49:08 +0300

    hw18.5

--- mantis_tests/src/test/java/ru/must/mantis/tests/UserRegistrationTests.java
+++ mantis_tests/src/test/java/ru/must/mantis/tests/UserRegistrationTests.java
@@ -14,18 +14,19 @@ public class UserRegistrationTests extends TestBase {
         // 1.Тест регистрирует новый адрес на почтовом сервере James.
         var username = String.format("%s", randomString(10));
         var email = String.format("%s@localhost", username);
-        app.jamesCliHelper().addUser(email, "password");
+        var password = "password";
+        app.jamesCliHelper().addUser(email, password);
         // 2.Пользователь с главной страницы начинает регистрацию.
         app.signupHelper().signup(username, email);
         // 3.1. Mantis отправляет письмо на указанный адрес, тест должен получить это письмо,
-        var messages = app.mailHelper().receive(email, "password", Duration.ofSeconds(10));
+        var messages = app.mailHelper().receive(email, password, Duration.ofSeconds(10));
         // 3.2. извлечь из него ссылку для подтверждения,
         var url = app.mailHelper().extractURL(messages);
         // 3.3. пройти по этой ссылке и завершить регистрацию.
         app.signupHelper().accountActivation(url);
-        app.signupHelper().editAccount(username,"password");
+        app.signupHelper().editAccount(username,password);
         // 4.Затем тест должен проверить, что пользователь может войти в систему с новым паролем. Этот шаг можно выполнить на уровне протокола HTTP.
-        app.httpSessionHelper().login(username, "password");
+        app.httpSessionHelper().login(username, password);
         Assertions.assertTrue(app.httpSessionHelper().isLoggedIn());
     }
 }
